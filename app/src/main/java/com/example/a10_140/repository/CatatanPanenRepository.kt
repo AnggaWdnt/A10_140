40e91747185721eb0e2e08c363f48c03a49a07ac
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-28 23:08:39 +0700

    penambahan code CatatanPanenRepository

--- app/src/main/java/com/example/a10_140/repository/CatatanPanenRepository.kt
+++ app/src/main/java/com/example/a10_140/repository/CatatanPanenRepository.kt
@@ -1,57 +1,44 @@
 package com.example.a10_140.repository
 
-import com.example.a10_140.model.Panen
-import com.example.a10_140.model.PanenDetailResponse
-import com.example.a10_140.model.PanenResponse
+import com.example.a10_140.model.CatatanPanen
 import com.example.a10_140.service.CatatanPanenService
 import java.io.IOException
 
 interface CatatanPanenRepository {
-    suspend fun getRiwayatPanen(): PanenResponse
-    suspend fun getPanenById(id: String): PanenDetailResponse
-    suspend fun insertCatatanPanen(panen: Panen)
-    suspend fun updateCatatanPanen(id: String, panen: Panen)
-    suspend fun deleteCatatanPanen(id: String)
+    suspend fun getCatatan(): List<CatatanPanen>
+    suspend fun insertCatatan(catatanPanen: CatatanPanen)
+    suspend fun updateCatatan(id: String, catatanPanen: CatatanPanen)
+    suspend fun deleteCatatan(id: String)
+    suspend fun getCatatanById(id: String): CatatanPanen
 }
 
-class NetworkCatatanPanenRepository(private val service: CatatanPanenService) : CatatanPanenRepository {
-    override suspend fun getRiwayatPanen(): PanenResponse {
-        return try {
-            service.getCatatanPanen()
-        } catch (e: IOException) {
-            throw IllegalStateException("Error: Unable to fetch Panen data: ${e.message}")
-        }
+class NetworkCatatanPanenRepository(
+    private val catatanPanenApiService: CatatanPanenService
+) : CatatanPanenRepository {
+    override suspend fun getCatatan(): List<CatatanPanen> = catatanPanenApiService.getCatatan()
+
+    override suspend fun insertCatatan(catatanPanen: CatatanPanen) {
+        catatanPanenApiService.insertCatatan(catatanPanen)
     }
 
-    override suspend fun getPanenById(id: String): PanenDetailResponse {
-        return try {
-            service.getCatatanPanenById(id)
-        } catch (e: IOException) {
-            throw IllegalStateException("Error: Unable to fetch Panen by ID: ${e.message}")
-        }
+    override suspend fun updateCatatan(id: String, catatanPanen: CatatanPanen) {
+        catatanPanenApiService.updateCatatan(id, catatanPanen)
     }
 
-    override suspend fun insertCatatanPanen(panen: Panen) {
+    override suspend fun deleteCatatan(id: String) {
         try {
-            service.insertCatatanPanen(panen)
-        } catch (e: IOException) {
-            throw IllegalStateException("Error: Unable to insert Panen: ${e.message}")
+            val response = catatanPanenApiService.deleteCatatan(id)
+            if (!response.isSuccessful) {
+                throw IOException("Gagal menghapus catatan panen. HTTP Status code: ${response.code()}")
+            } else {
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
         }
     }
 
-    override suspend fun updateCatatanPanen(id: String, panen: Panen) {
-        try {
-            service.editCatatanPanen(id, panen)
-        } catch (e: IOException) {
-            throw IllegalStateException("Error: Unable to update Panen: ${e.message}")
-        }
-    }
-
-    override suspend fun deleteCatatanPanen(id: String) {
-        try {
-            service.deleteCatatanPanen(id)
-        } catch (e: IOException) {
-            throw IllegalStateException("Error: Unable to delete Panen: ${e.message}")
-        }
+    override suspend fun getCatatanById(id: String): CatatanPanen {
+        return catatanPanenApiService.getCatatanById(id)
     }
 }
