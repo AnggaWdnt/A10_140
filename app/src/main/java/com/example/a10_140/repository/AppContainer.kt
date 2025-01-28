216c35de73f52f10dddbe6c7d7f6af2ff2bf34e9
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-28 15:40:09 +0700

    penambahan code AppContainer pada package repository

---
+++ app/src/main/java/com/example/a10_140/repository/AppContainer.kt
@@ -0,0 +1,42 @@
+package com.example.a10_140.repository
+
+import com.example.a10_140.service.*
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+
+interface AppContainer {
+    val tanamanRepository: TanamanRepository
+    val pekerjaRepository: PekerjaRepository
+    val catatanPanenRepository: CatatanPanenRepository
+    val aktivitasPertanianRepository: AktivitasPertanianRepository
+}
+
+
+class AppContainerImpl : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:3000/api/"
+
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    // Services
+    private val tanamanService: TanamanService by lazy { retrofit.create(TanamanService::class.java) }
+    private val pekerjaService: PekerjaService by lazy { retrofit.create(PekerjaService::class.java) }
+    private val catatanPanenService: CatatanPanenService by lazy { retrofit.create(CatatanPanenService::class.java) }
+    private val aktivitasPertanianService: AktivitasPertanianService by lazy { retrofit.create(AktivitasPertanianService::class.java) }
+
+    // Repositories
+    override val tanamanRepository: TanamanRepository by lazy { NetworkTanamanRepository(tanamanService) }
+    override val pekerjaRepository: PekerjaRepository by lazy { NetworkPekerjaRepository(pekerjaService) }
+    override val catatanPanenRepository: CatatanPanenRepository by lazy { NetworkCatatanPanenRepository(catatanPanenService) }
+    override val aktivitasPertanianRepository: AktivitasPertanianRepository by lazy { NetworkAktivitasPertanianRepository(aktivitasPertanianService) }
+}
