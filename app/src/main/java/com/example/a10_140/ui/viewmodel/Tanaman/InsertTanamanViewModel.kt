f153cc509281bd3ffdfbd86f5a02182335129152
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-28 19:33:38 +0700

    perbaikan code InsertTanamanViewModel

---
+++ app/src/main/java/com/example/a10_140/ui/viewmodel/Tanaman/InsertTanamanViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.a10_140.ui.viewmodel.Tanaman
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a10_140.model.Tanaman
+import com.example.a10_140.repository.TanamanRepository
+import kotlinx.coroutines.launch
+
+
+class InsertTanamanViewModel (private val tanaman: TanamanRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertTanamanState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertTanaman(){
+        viewModelScope.launch {
+            try {
+                tanaman.insertTanaman(uiState.insertUiEvent.toTanaman())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+
+data class InsertUiState(
+    val insertUiEvent : InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val idTanaman: String = "",
+    val namaTanaman: String = "",
+    val periodeTanaman: String = "",
+    val deskripsiTanaman: String = ""
+)
+
+fun InsertUiEvent.toTanaman(): Tanaman = Tanaman(
+    idTanaman = idTanaman,
+    namaTanaman = namaTanaman,
+    periodeTanaman = periodeTanaman,
+    deskripsiTanaman = deskripsiTanaman
+)
+
+fun Tanaman.toUiStateTanaman():InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Tanaman.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    idTanaman = idTanaman,
+    namaTanaman = namaTanaman,
+    periodeTanaman = periodeTanaman,
+    deskripsiTanaman = deskripsiTanaman
+)
