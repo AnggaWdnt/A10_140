335f95793b71cb311e78d603b8836d94571d61ff
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-01-28 23:28:38 +0700

    penambahan code HomeAktivitasViewModel

--- app/src/main/java/com/example/a10_140/ui/viewmodel/AktivitasPertanian/HomeAktivitasViewModel.kt
+++ app/src/main/java/com/example/a10_140/ui/viewmodel/AktivitasPertanian/HomeAktivitasViewModel.kt
@@ -1,2 +1,53 @@
 package com.example.a10_140.ui.viewmodel.AktivitasPertanian
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a10_140.model.Aktivitaspertanian
+import com.example.a10_140.repository.AktivitasPertanianRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+
+
+sealed class AktivitasUiState{
+    data class Success(val aktivitas: List<Aktivitaspertanian>): AktivitasUiState()
+    object Error: AktivitasUiState()
+    object Loading: AktivitasUiState()
+}
+
+class HomeAktivitasViewModel (private val aktivitas: AktivitasPertanianRepository): ViewModel(){
+    var aktivitasUiState: AktivitasUiState by mutableStateOf(AktivitasUiState.Loading)
+        private set
+
+    init {
+        getaktivitas()
+    }
+
+    fun getaktivitas(){
+        viewModelScope.launch {
+            aktivitasUiState = AktivitasUiState.Loading
+            aktivitasUiState = try {
+                AktivitasUiState.Success(aktivitas.getDaftarAktivitas())
+            }catch (e:IOException){
+                AktivitasUiState.Error
+            }catch (e: HttpException){
+                AktivitasUiState.Error
+            }
+        }
+    }
+
+    fun deleteAktivitas(id:String){
+        viewModelScope.launch {
+            try {
+                aktivitas.deleteAktivitas(id)
+            }catch (e: IOException){
+                AktivitasUiState.Error
+            }catch (e: HttpException){
+                AktivitasUiState.Error
+            }
+        }
+    }
+}
